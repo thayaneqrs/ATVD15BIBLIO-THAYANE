7ea7671db0191624f4ebb972d1d0a9c67c8108a8
FRANCISCA THAYANE QUEIROS DE OLIVEIRA <20231094010058@ifrn.local>
2026-08-27 09:50:44 -0300

    modelo acervo

--- acervo/models.py
+++ acervo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Acervo(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True, null=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
